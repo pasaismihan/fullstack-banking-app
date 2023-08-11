c9a5abdec31683d51b6396e63b0ec57307edf19b
pasaismihan <pasa_13142@hotmail.com>
2023-08-11 12:46:46 +0300

    Login form olusturuldu

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,18 +3,87 @@ import { connect } from "react-redux";
 import { Form, Button } from "react-bootstrap";
 import { validateFields } from "../utils/common";
 import { Link } from "react-router-dom";
+import { render } from "node-sass";
 
 class Login extends React.Component {
   state = {
     email: "",
     password: "",
-    errormsg: "",
+    errorMsg: "",
   };
+
+  handleLogin = (event) => {
+    event.preventDefault();
+    const { email, password } = this.state;
+    const fieldsToValidate = [{ email }, { password }];
+    const allFieldsEntered = validateFields(fieldsToValidate);
+
+    if (!allFieldsEntered) {
+      this.setState({
+        errorMsg: {
+          signin_error: "Please enter all the fields.",
+        },
+      });
+    } else {
+      this.setState({
+        errorMsg: {
+          signin_error: "",
+        },
+      });
+      //login succesful
+    }
+  };
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  render() {
+    const { errorMsg } = this.state;
+    return (
+      <div className="login-page">
+        <h1>Banking Application</h1>
+        <div className="login-form">
+          <Form onSubmit={this.handleLogin}>
+            {errorMsg && errorMsg.signin_error && (
+              <p className="errorMsg centered-message">
+                {errorMsg.signin_error}
+              </p>
+            )}
+            <Form.Group controlId="email">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                name="email"
+                placeholder="Enter email"
+                onChange={this.handleInputChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                name="password"
+                placeholder="Enter password"
+                onChange={this.handleInputChange}
+              />
+            </Form.Group>
+            <div className="action-items">
+              <Button variant="primary" type="submit">
+                Login
+              </Button>
+              <Link to="/register" className="btn btn-secondary">
+                Create account
+              </Link>
+            </div>
+          </Form>
+        </div>
+      </div>
+    );
+  }
 }
 
-const handleLogin = (event) => {
-  event.preventDefault();
-  const { email, password } = this.state;
-  const fieldsToValidate = [{ email }, { password }];
-  const allFieldsEntered = validateFields(fieldsToValidate);
-};
+export default connect()(Login);
